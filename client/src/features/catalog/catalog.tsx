ef13952f2f1f3ec3e6f92e73cc9b82e8e7006aeb
Himanshu Tomar <htomar@raintreeinc.com>
2022-05-26 14:48:12 +0530

    Material UI changes

---
+++ client/src/features/catalog/catalog.tsx
@@ -0,0 +1,18 @@
+import { Fragment, useEffect, useState } from "react"
+import { Product } from "../../app/models/product"
+import ProductList from "./ProductList";
+
+
+
+export default function Catalog() {
+    // lets destructure the property 
+    const [products, setProducts] = useState<Product[]>([]);
+    useEffect(() => {
+      fetch("http://localhost:5000/api/products").then(response => response.json()).then(data => setProducts(data))
+    },[]);
+        return (
+        <>
+            <ProductList products={products} />
+        </>
+    )
+}
